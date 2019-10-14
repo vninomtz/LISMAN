2e9e7aedd4e558929d036ad580145432a20208f8
Victor Niño <ninomtz.victor@gmail.com>
2019-10-13 22:56:14 -0500

    Implementación intefaz de juego server

---
+++ LismanService/LismanService/IGameManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace LismanService {
+
+    [ServiceContract (CallbackContract = typeof(IGameManagerCallBack))]
+    public interface IGameManager {
+        [OperationContract(IsOneWay = true)]
+        void CreateGame(String user);
+        [OperationContract(IsOneWay = true)]
+        void JoinGame(String user);
+        [OperationContract(IsOneWay = true)]
+        void LeaveGame(String user, int game);
+    }
+
+    [ServiceContract]
+    public interface IGameManagerCallBack {
+        [OperationContract(IsOneWay = true)]
+        void NotifyJoinedUser(String user);
+        [OperationContract(IsOneWay = true)]
+        void NotifyJoined(int game, String user);
+
+        [OperationContract(IsOneWay = true)]
+        void NotifyErrorMessage(int error);
+        [OperationContract(IsOneWay = true)]
+        void NotifyLeaveUser(String user);
+        [OperationContract(IsOneWay = true)]
+        void NotifyGameCreated(int message);
+
+    }
+}
---
+++ LismanService/LismanService/LoginManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Validation;
+using DataAccess;
+using System.ServiceModel;
+
+
+namespace LismanService {
+    public partial class LismanService : ILoginManager {
+        public bool EmailExists(string emailAdress)
+        {
+            try {
+                using (var dataBase = new EntityModelContainer()) {
+                    int exists = dataBase.PlayerSet.Where(u => u.Email == emailAdress).Count();
+                    if (exists > 0) {
+                        return true;
+                    }
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            return false;
+        }
+        public Account LoginAccount(string user, string password)
+        {
+            try {
+                using (var dataBase = new EntityModelContainer()) {
+                    int exists = dataBase.AccountSet.Where(u => u.User == user & u.Password == password).Count();
+                    if (exists > 0) {
+                        return dataBase.AccountSet.Where(u => u.User == user & u.Password == password).Select(u => new Account
+                        {
+                            Id = u.Id,
+                            User = u.User,
+                            Password = u.Password,
+                            Registration_date = u.Registration_date,
+                            Key_confirmation = u.Key_confirmation
+                        }).FirstOrDefault();
+                    } else {
+                        return null;
+                    }
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+
+        public bool UserNameExists(string username)
+        {
+            try {
+                using (var dataBase = new EntityModelContainer()) {
+                    int exists = dataBase.AccountSet.Where(u => u.User == username).Count();
+                    if (exists > 0) {
+                        return true;
+                    }
+                }
+            } catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            return false;
+        }
+    }
+}

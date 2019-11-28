cc044ce1729b6ceec607eeda80acfb04faa5a855
AlanGlezH <47785262+AlanGlezH@users.noreply.github.com>
2019-11-28 08:03:37 -0600

    Inicio de correción deuda técnica

--- LismanService/LismanService/GameManager.cs
+++ LismanService/LismanService/GameManager.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ServiceModel;
-using System.Runtime.Serialization;
 using DataAccess;
 using System.Data.Entity.Validation;
 
